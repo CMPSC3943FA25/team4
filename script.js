b40528104f952ce93b881875f608026db9eb7523
Halowar05 <130316406+Halowar05@users.noreply.github.com>
2025-11-01 15:02:26 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+function saveData() {
+    const inputValue = document.getElementById('userInput').value;
 
+    localStorage.setItem('userData', inputValue);
+
+    displaySavedData();
+}
+
+function displaySavedData() { 
+    const saveData = localStorage.getItem('userData');
+    const saveDataElement = document.getElementById('saveData');
+    if (saveData){
+        saveDataElement.textContent = saveData;
+    } else {
+        saveDataElement.textContent = "no data saved";
+    }
+}
+window.onload = displaySavedData
+window.onclose = saveData
